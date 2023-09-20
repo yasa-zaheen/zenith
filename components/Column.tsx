367d0b8839ca401104b252959a3c1921e4e6c524
Yasa Zaheen <yasazaheen@gmail.com>
2023-09-20 12:36:05 +0600

    UI

--- components/Column.tsx
+++ components/Column.tsx
@@ -42,7 +42,7 @@ function Column({
               {...provided.droppableProps}
               className={`${
                 snapShot.isUsingPlaceholder ? "bg-neutral-200" : ""
-              } `}
+              } mb-8 md:mb-0 `}
             >
               {tasks.map((task, index) => (
                 <Draggable key={task.id} draggableId={task.id} index={index}>
